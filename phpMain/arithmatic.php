0789532fba9b91c2b6c5cc0011477c7eef277c34
DanielNMartin <dan.martin@sainsburys.co.uk>
2017-09-15 14:51:46 +0100

    Re-structured code with correct Linting

--- phpMain/arithmatic.php
+++ phpMain/arithmatic.php
@@ -1,30 +1,26 @@
 <?php
 
-function addition ($a, $b) {
-  $sum= $a + $b;
+function addition($a, $b) {
+  $sum = $a + $b;
   return $sum;
-  }
+}
 
-echo addition (6, 2) ."\n";
-
-
-function subtraction ($a, $b) {
-  $min= $a - $b;
+function subtraction($a, $b) {
+  $min = $a - $b;
   return $min;
-  }
+}
 
-echo subtraction (5, 2) ."\n";
-
-function multiplication ($a, $b) {
-  $mul= $a * $b;
+function multiplication($a, $b) {
+  $mul = $a * $b;
   return $mul;
 }
 
-echo multiplication (8, 2) ."\n";
-
-function division ($a, $b) {
-  $div= $a / $b;
+function division($a, $b) {
+  $div = $a / $b;
   return $div;
 }
 
+echo addition (6, 2) ."\n";
+echo subtraction (5, 2) ."\n";
+echo multiplication (8, 2) ."\n";
 echo division (9, 3) ."\n";
