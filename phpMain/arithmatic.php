7df3cec3fe2cd7f28f4875142dbcc71de08cc519
DanielNMartin <dan.martin@sainsburys.co.uk>
2017-09-15 15:08:51 +0100

    New format for improved Data Ootput

--- phpMain/arithmatic.php
+++ phpMain/arithmatic.php
@@ -1,26 +1,26 @@
 <?php
 
-function addition($a, $b) {
+function addition($a,$b) {
   $sum = $a + $b;
   return $sum;
 }
 
-function subtraction($a, $b) {
+function subtraction($a,$b) {
   $min = $a - $b;
   return $min;
 }
 
-function multiplication($a, $b) {
+function multiplication($a,$b) {
   $mul = $a * $b;
   return $mul;
 }
 
-function division($a, $b) {
+function division($a,$b) {
   $div = $a / $b;
   return $div;
 }
 
-echo addition (6, 2) ."\n";
-echo subtraction (5, 2) ."\n";
-echo multiplication (8, 2) ."\n";
-echo division (9, 3) ."\n";
+echo "Sum of 6 + 2 = " . addition (6, 2) ."\n";
+echo "Subtraction of 5 - 2 = " . subtraction (5, 2) ."\n";
+echo "Multiplication of 8 * 2 = " . multiplication (8, 2) ."\n";
+echo "Division of 9 / 3 = " . division (9, 3) ."\n";
